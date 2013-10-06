6f294c6f587f13c09bc6edd72c2f5deac55a0838
wcdolphin <wcdolphin@gmail.com>
2013-10-06 01:07:13 -0400

    Adds Maps dropdown menu

--- js/views/CreateDestinationView.js
+++ js/views/CreateDestinationView.js
@@ -12,26 +12,31 @@ app.views.CreateDestinationView = Backbone.View.extend({
     },
 
     init_map: function(){
-    var win = function(position) {
-        var lat = position.coords.latitude;
-        var long = position.coords.longitude;
-        var myLatlng = new google.maps.LatLng(lat, long);
+        var win = function(position) {
+            var lat = position.coords.latitude;
+            var long = position.coords.longitude;
+            var myLatlng = new google.maps.LatLng(lat, long);
 
-        var myOptions = {
-            center: myLatlng,
-            zoom: 16,
-            mapTypeId: google.maps.MapTypeId.HYBRID
+            var myOptions = {
+                center: myLatlng,
+                zoom: 16,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
+            map_element = document.getElementById("map_canvas");
+            map = new google.maps.Map(map_element, myOptions);
         };
-        map_element = document.getElementById("map_canvas");
-        map = new google.maps.Map(map_element, myOptions);
-    };
 
-    var fail = function(e) {
-        $.mobile.hidePageLoadingMsg();
-        alert('Can\'t retrieve position.\nError: ' + e);
-    };
+        var fail = function(e) {
+            $.mobile.hidePageLoadingMsg();
+            alert('Can\'t retrieve position.\nError: ' + e);
+        };
 
-    watchID = navigator.geolocation.getCurrentPosition(win, fail);
+        watchID = navigator.geolocation.getCurrentPosition(win, fail);
+        console.log("changed");
+        var input = document.getElementById('search-locations');
+        console.log(input);
+        var searchBox = new google.maps.places.SearchBox(input);
+
     }
 
 });
